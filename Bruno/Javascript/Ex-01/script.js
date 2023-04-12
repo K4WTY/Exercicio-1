0bf51591093a349a14f83de95bf886f3f29b4411
K4WTY <eduardo.schogor.ruiz.profissional@gmail.com>
2023-04-11 21:57:17 -0300

    aaaa

---
+++ Bruno/Javascript/Ex-01/script.js
@@ -0,0 +1,8 @@
+const name = prompt('Digite seu primeiro nome:')
+const lastName = prompt('Digite seu sobrenome:')
+const campName = prompt('Digite seu campo de estudo:')
+const year = prompt('Digite seu ano de nascimento:')
+
+alert(`Nome completo: ${name} ${lastName} 
+Campo de estudo: ${campName} 
+Idade: ${(2023 - year)} anos`)
---
+++ Bruno/Javascript/Ex-02/script.js
@@ -0,0 +1,12 @@
+const number_1 = parseFloat(prompt('Digite um número:'))
+const number_2 = parseFloat(prompt('Digite outro número:'))
+
+let soma = number_1 + number_2
+let subt = number_1 - number_2
+let mult = number_1 * number_2
+let div = number_1 / number_2
+
+alert(`A soma entre esses dois números é: ${soma}
+A subtração entre esses dois números é: ${subt}
+A multiplicação entre esses dois números é: ${mult}
+A divisão entre esses dois números é: ${div}`)
